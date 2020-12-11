afa2be50bfce42d5d3b91b7eab109e220ef6f093
DEV <dev@info.local>
2020-12-11 16:18:46 -0300

    Elevador

---
+++ Classes/Elevador.cs
@@ -0,0 +1,14 @@
+namespace POO_Polimorfismo_Elevador.Classes
+{
+    public class Elevador
+    {
+        public int andarAtual = 0;
+        public int andaresNoPredio;
+        public string capacidade;
+        public string pessoasNoElevador;
+        public string Inicializa(int andarAtual, string capacidade, int andaresNoPredio)
+        {
+            return "";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace POO_Polimorfismo_Elevador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
